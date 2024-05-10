fa40f0b8ecc547973614e57cb70e9251ae938024
user50lab <56128974+user50lab@users.noreply.github.com>
2024-05-10 17:03:27 +0800

    Update ppo.py

--- Ours/ppo.py
+++ Ours/ppo.py
@@ -26,7 +26,7 @@ print("=========================================================================
 device = torch.device('cpu')
 
 if (torch.cuda.is_available()):
-    device = torch.device('cuda:2')
+    device = torch.device('cuda:0')
     torch.cuda.empty_cache()
     print("Device set to : " + str(torch.cuda.get_device_name(device)))
 else:
